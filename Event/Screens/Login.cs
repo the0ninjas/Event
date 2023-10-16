a3f9e1a3410bc98dc7cd9f4c0027f94554338c1e
uuuta714 <horiyuuu@gmail.com>
2023-10-16 16:06:59 +1100

    Updated Login page

---
+++ Event/Screens/Login.cs
@@ -0,0 +1,34 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Event.Screens
+{
+    public partial class Login : MaterialForm
+    {
+        public Login()
+        {
+            // Manage colorscheme of the graphical user interface
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.BlueGrey800,
+                Primary.BlueGrey900,
+                Primary.BlueGrey500,
+                Accent.LightBlue200,
+                TextShade.WHITE
+            );
+
+        }
+    }
+}
