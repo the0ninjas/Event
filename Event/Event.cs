82dc03a3266fb4d0f081aa76ccd51cda7c34b60b
HPaaron <114461908+HPaaron@users.noreply.github.com>
2023-10-18 17:46:56 +1100

    Added properties to user and event class

---
+++ Event/Event.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem
+{
+    internal class Event
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+        public List<User> Attendees { get; set; }
+
+        public Event(string name, DateTime date, DateTime time, string location, string description, bool isVisible)
+        {
+            Name = name;
+            Date = date;
+            Time = time;
+            Location = location;
+            Description = description;
+            IsVisible = isVisible;
+            Attendees = new List<User>();
+        }
+
+        // Method to add an attendee to the event
+        public void AddAttendee(User user)
+        {
+            Attendees.Add(user);
+        }
+
+        // Method to remove an attendee from the event
+        public void RemoveAttendee(User user)
+        {
+            Attendees.Remove(user);
+        }
+
+
+    }
+}
