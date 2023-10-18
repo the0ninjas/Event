8adbc820e4ed23daf4623db4bcf1546d563be0ac
Stanley Tsz Kin Lam <StanleyTszKin.Lam@student.uts.edu.au>
2023-10-18 17:16:57 +1100

    Added class for User, Event, Notification

---
+++ Event/Notification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem
+{
+    internal class Notification
+    {
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool IsRead { get; set; }
+
+        public Notification(string message)
+        {
+            Message = message;
+            Timestamp = DateTime.Now;
+            IsRead = false; // By default, a notification is unread.
+        }
+
+        // Method to mark the notification as read
+        public void MarkAsRead()
+        {
+            IsRead = true;
+        }
+    }
+}
---
+++ Event/User.cs
@@ -0,0 +1,58 @@
+﻿using EventManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem
+{
+    internal class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public List<Event> CreatedEvents { get; set; }
+        public List<Event> JoinedEvents { get; set; }
+        public List<Notification> Notifications { get; set; }
+
+        public User(string username, string password, string email)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            CreatedEvents = new List<Event>();
+            JoinedEvents = new List<Event>();
+            Notifications = new List<Notification>();
+        }
+
+        // Method to create a new event
+        public void CreateEvent(string name, DateTime date, DateTime time, string location, string description, bool isVisible)
+        {
+            Event newEvent = new Event(name, date, time, location, description, isVisible);
+            CreatedEvents.Add(newEvent);
+        }
+
+        // Method to join an event
+        public void JoinEvent(Event eventToJoin)
+        {
+            eventToJoin.AddAttendee(this);
+            JoinedEvents.Add(eventToJoin);
+        }
+
+        // Method to leave an event
+        public void LeaveEvent(Event eventToLeave)
+        {
+            eventToLeave.RemoveAttendee(this);
+            JoinedEvents.Remove(eventToLeave);
+        }
+
+        // Method to send a notification
+        public void SendNotification(string message)
+        {
+            Notification notification = new Notification(message);
+            Notifications.Add(notification);
+        }
+    }
+}
+
