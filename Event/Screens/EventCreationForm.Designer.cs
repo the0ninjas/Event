03937da87bbb8878d0fb8c478f1e0650ec52fe23
uuuta714 <horiyuuu@gmail.com>
2023-10-22 21:45:28 +1100

    Updated UserProfile.cs and EventCreationForm.cs

--- Event/Screens/EventCreationForm.Designer.cs
+++ Event/Screens/EventCreationForm.Designer.cs
@@ -28,67 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            label5 = new Label();
-            label6 = new Label();
+            headingLabel = new Label();
+            titleLabel = new Label();
+            dateLabel = new Label();
+            timeLabel = new Label();
+            locationLabel = new Label();
+            descriptionLabel = new Label();
+            capacityLabel = new Label();
+            titleTextBox = new TextBox();
+            datePicker = new DateTimePicker();
+            timePicker = new DateTimePicker();
+            locationComboBox = new ComboBox();
+            capacityTextBox = new TextBox();
+            descriptionTextBox = new TextBox();
+            createEventPanel = new Panel();
+            submitButton = new Button();
+            createEventPanel.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // headingLabel
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(343, 207);
-            label1.Name = "label1";
-            label1.Size = new Size(68, 30);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
+            headingLabel.AutoSize = true;
+            headingLabel.Font = new Font("Segoe UI Black", 15.000001F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            headingLabel.Location = new Point(168, 57);
+            headingLabel.Name = "headingLabel";
+            headingLabel.Size = new Size(246, 48);
+            headingLabel.TabIndex = 0;
+            headingLabel.Text = "Create Event";
             // 
-            // label2
+            // titleLabel
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(343, 282);
-            label2.Name = "label2";
-            label2.Size = new Size(68, 30);
-            label2.TabIndex = 1;
-            label2.Text = "label2";
+            titleLabel.AutoSize = true;
+            titleLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
+            titleLabel.Location = new Point(168, 158);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new Size(61, 32);
+            titleLabel.TabIndex = 1;
+            titleLabel.Text = "Title";
             // 
-            // label3
+            // dateLabel
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(343, 349);
-            label3.Name = "label3";
-            label3.Size = new Size(68, 30);
-            label3.TabIndex = 2;
-            label3.Text = "label3";
+            dateLabel.AutoSize = true;
+            dateLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
+            dateLabel.Location = new Point(168, 258);
+            dateLabel.Name = "dateLabel";
+            dateLabel.Size = new Size(66, 32);
+            dateLabel.TabIndex = 2;
+            dateLabel.Text = "Date";
             // 
-            // label4
+            // timeLabel
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(343, 419);
-            label4.Name = "label4";
-            label4.Size = new Size(68, 30);
-            label4.TabIndex = 3;
-            label4.Text = "label4";
+            timeLabel.AutoSize = true;
+            timeLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
+            timeLabel.Location = new Point(168, 358);
+            timeLabel.Name = "timeLabel";
+            timeLabel.Size = new Size(67, 32);
+            timeLabel.TabIndex = 3;
+            timeLabel.Text = "Time";
             // 
-            // label5
+            // locationLabel
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(343, 480);
-            label5.Name = "label5";
-            label5.Size = new Size(68, 30);
-            label5.TabIndex = 4;
-            label5.Text = "label5";
+            locationLabel.AutoSize = true;
+            locationLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
+            locationLabel.Location = new Point(168, 458);
+            locationLabel.Name = "locationLabel";
+            locationLabel.Size = new Size(107, 32);
+            locationLabel.TabIndex = 4;
+            locationLabel.Text = "Location";
             // 
-            // label6
+            // descriptionLabel
             // 
-            label6.AutoSize = true;
-            label6.Location = new Point(343, 543);
-            label6.Name = "label6";
-            label6.Size = new Size(68, 30);
-            label6.TabIndex = 5;
-            label6.Text = "label6";
+            descriptionLabel.AutoSize = true;
+            descriptionLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
+            descriptionLabel.Location = new Point(168, 658);
+            descriptionLabel.Name = "descriptionLabel";
+            descriptionLabel.Size = new Size(137, 32);
+            descriptionLabel.TabIndex = 5;
+            descriptionLabel.Text = "Description";
+            // 
+            // capacityLabel
+            // 
+            capacityLabel.AutoSize = true;
+            capacityLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
+            capacityLabel.Location = new Point(168, 558);
+            capacityLabel.Name = "capacityLabel";
+            capacityLabel.Size = new Size(161, 32);
+            capacityLabel.TabIndex = 6;
+            capacityLabel.Text = "Max Capacity";
+            // 
+            // titleTextBox
+            // 
+            titleTextBox.Location = new Point(405, 158);
+            titleTextBox.Name = "titleTextBox";
+            titleTextBox.Size = new Size(350, 35);
+            titleTextBox.TabIndex = 7;
+            // 
+            // datePicker
+            // 
+            datePicker.Format = DateTimePickerFormat.Short;
+            datePicker.Location = new Point(405, 258);
+            datePicker.Name = "datePicker";
+            datePicker.Size = new Size(350, 35);
+            datePicker.TabIndex = 8;
+            // 
+            // timePicker
+            // 
+            timePicker.CustomFormat = "hh:mm tt";
+            timePicker.Format = DateTimePickerFormat.Custom;
+            timePicker.Location = new Point(405, 358);
+            timePicker.Name = "timePicker";
+            timePicker.ShowUpDown = true;
+            timePicker.Size = new Size(350, 35);
+            timePicker.TabIndex = 9;
+            // 
+            // locationComboBox
+            // 
+            locationComboBox.FormattingEnabled = true;
+            locationComboBox.Items.AddRange(new object[] { "Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide", "Canberra", "Hobart", "Darwin" });
+            locationComboBox.Location = new Point(405, 458);
+            locationComboBox.Name = "locationComboBox";
+            locationComboBox.Size = new Size(350, 38);
+            locationComboBox.TabIndex = 10;
+            // 
+            // capacityTextBox
+            // 
+            capacityTextBox.Location = new Point(405, 558);
+            capacityTextBox.Name = "capacityTextBox";
+            capacityTextBox.Size = new Size(350, 35);
+            capacityTextBox.TabIndex = 11;
+            // 
+            // descriptionTextBox
+            // 
+            descriptionTextBox.Location = new Point(405, 658);
+            descriptionTextBox.Multiline = true;
+            descriptionTextBox.Name = "descriptionTextBox";
+            descriptionTextBox.Size = new Size(350, 118);
+            descriptionTextBox.TabIndex = 12;
+            // 
+            // createEventPanel
+            // 
+            createEventPanel.Controls.Add(submitButton);
+            createEventPanel.Controls.Add(timePicker);
+            createEventPanel.Controls.Add(descriptionTextBox);
+            createEventPanel.Controls.Add(headingLabel);
+            createEventPanel.Controls.Add(capacityTextBox);
+            createEventPanel.Controls.Add(titleLabel);
+            createEventPanel.Controls.Add(locationComboBox);
+            createEventPanel.Controls.Add(dateLabel);
+            createEventPanel.Controls.Add(timeLabel);
+            createEventPanel.Controls.Add(datePicker);
+            createEventPanel.Controls.Add(locationLabel);
+            createEventPanel.Controls.Add(titleTextBox);
+            createEventPanel.Controls.Add(descriptionLabel);
+            createEventPanel.Controls.Add(capacityLabel);
+            createEventPanel.Location = new Point(354, 45);
+            createEventPanel.Name = "createEventPanel";
+            createEventPanel.Size = new Size(916, 939);
+            createEventPanel.TabIndex = 13;
+            // 
+            // submitButton
+            // 
+            submitButton.BackColor = Color.DarkSeaGreen;
+            submitButton.FlatStyle = FlatStyle.Popup;
+            submitButton.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            submitButton.Location = new Point(405, 836);
+            submitButton.Name = "submitButton";
+            submitButton.Size = new Size(131, 40);
+            submitButton.TabIndex = 13;
+            submitButton.Text = "Submit";
+            submitButton.UseVisualStyleBackColor = false;
             // 
             // EventCreationForm
             // 
@@ -96,25 +204,30 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.MintCream;
             ClientSize = new Size(1622, 1122);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(createEventPanel);
             Name = "EventCreationForm";
             Text = "Event Creation Form";
+            createEventPanel.ResumeLayout(false);
+            createEventPanel.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private Label label2;
-        private Label label3;
-        private Label label4;
-        private Label label5;
-        private Label label6;
+        private Label headingLabel;
+        private Label titleLabel;
+        private Label dateLabel;
+        private Label timeLabel;
+        private Label locationLabel;
+        private Label descriptionLabel;
+        private Label capacityLabel;
+        private TextBox titleTextBox;
+        private DateTimePicker datePicker;
+        private DateTimePicker timePicker;
+        private ComboBox locationComboBox;
+        private TextBox capacityTextBox;
+        private TextBox descriptionTextBox;
+        private Panel createEventPanel;
+        private Button submitButton;
     }
 }
--- Event/Screens/UserProfile.Designer.cs
+++ Event/Screens/UserProfile.Designer.cs
@@ -34,14 +34,21 @@
             emailLabel = new Label();
             phoneLabel = new Label();
             locationLabel = new Label();
-            notificationLabel = new Label();
+            panel1 = new Panel();
+            firstNameTextBox = new TextBox();
+            lastNameTextBox = new TextBox();
+            emailTextBox = new TextBox();
+            phoneTextBox = new TextBox();
+            locationComboBox = new ComboBox();
+            updateButton = new Button();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // headingLabel
             // 
             headingLabel.AutoSize = true;
             headingLabel.Font = new Font("Segoe UI Black", 15.000001F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            headingLabel.Location = new Point(154, 145);
+            headingLabel.Location = new Point(154, 75);
             headingLabel.Name = "headingLabel";
             headingLabel.Size = new Size(230, 48);
             headingLabel.TabIndex = 0;
@@ -51,7 +58,7 @@
             // 
             firstNameLabel.AutoSize = true;
             firstNameLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            firstNameLabel.Location = new Point(669, 250);
+            firstNameLabel.Location = new Point(154, 236);
             firstNameLabel.Name = "firstNameLabel";
             firstNameLabel.Size = new Size(132, 32);
             firstNameLabel.TabIndex = 1;
@@ -61,7 +68,7 @@
             // 
             lastNameLabel.AutoSize = true;
             lastNameLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            lastNameLabel.Location = new Point(669, 350);
+            lastNameLabel.Location = new Point(154, 336);
             lastNameLabel.Name = "lastNameLabel";
             lastNameLabel.Size = new Size(130, 32);
             lastNameLabel.TabIndex = 2;
@@ -71,7 +78,7 @@
             // 
             emailLabel.AutoSize = true;
             emailLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            emailLabel.Location = new Point(669, 450);
+            emailLabel.Location = new Point(154, 436);
             emailLabel.Name = "emailLabel";
             emailLabel.Size = new Size(165, 32);
             emailLabel.TabIndex = 3;
@@ -81,7 +88,7 @@
             // 
             phoneLabel.AutoSize = true;
             phoneLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            phoneLabel.Location = new Point(669, 550);
+            phoneLabel.Location = new Point(154, 536);
             phoneLabel.Name = "phoneLabel";
             phoneLabel.Size = new Size(179, 32);
             phoneLabel.TabIndex = 4;
@@ -91,21 +98,79 @@
             // 
             locationLabel.AutoSize = true;
             locationLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            locationLabel.Location = new Point(669, 650);
+            locationLabel.Location = new Point(154, 636);
             locationLabel.Name = "locationLabel";
             locationLabel.Size = new Size(107, 32);
             locationLabel.TabIndex = 5;
             locationLabel.Text = "Location";
             // 
-            // notificationLabel
+            // panel1
             // 
-            notificationLabel.AutoSize = true;
-            notificationLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            notificationLabel.Location = new Point(669, 750);
-            notificationLabel.Name = "notificationLabel";
-            notificationLabel.Size = new Size(142, 32);
-            notificationLabel.TabIndex = 6;
-            notificationLabel.Text = "Notification";
+            panel1.Controls.Add(updateButton);
+            panel1.Controls.Add(locationComboBox);
+            panel1.Controls.Add(phoneTextBox);
+            panel1.Controls.Add(emailTextBox);
+            panel1.Controls.Add(lastNameTextBox);
+            panel1.Controls.Add(firstNameTextBox);
+            panel1.Controls.Add(emailLabel);
+            panel1.Controls.Add(headingLabel);
+            panel1.Controls.Add(firstNameLabel);
+            panel1.Controls.Add(locationLabel);
+            panel1.Controls.Add(lastNameLabel);
+            panel1.Controls.Add(phoneLabel);
+            panel1.Location = new Point(304, 45);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1016, 855);
+            panel1.TabIndex = 7;
+            // 
+            // firstNameTextBox
+            // 
+            firstNameTextBox.Location = new Point(458, 236);
+            firstNameTextBox.Name = "firstNameTextBox";
+            firstNameTextBox.Size = new Size(414, 35);
+            firstNameTextBox.TabIndex = 7;
+            // 
+            // lastNameTextBox
+            // 
+            lastNameTextBox.Location = new Point(458, 336);
+            lastNameTextBox.Name = "lastNameTextBox";
+            lastNameTextBox.Size = new Size(414, 35);
+            lastNameTextBox.TabIndex = 8;
+            // 
+            // emailTextBox
+            // 
+            emailTextBox.Location = new Point(458, 436);
+            emailTextBox.Name = "emailTextBox";
+            emailTextBox.Size = new Size(414, 35);
+            emailTextBox.TabIndex = 9;
+            // 
+            // phoneTextBox
+            // 
+            phoneTextBox.Location = new Point(458, 536);
+            phoneTextBox.Name = "phoneTextBox";
+            phoneTextBox.Size = new Size(414, 35);
+            phoneTextBox.TabIndex = 10;
+            // 
+            // locationComboBox
+            // 
+            locationComboBox.FormattingEnabled = true;
+            locationComboBox.Items.AddRange(new object[] { "Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide", "Canberra", "Hobart", "Darwin" });
+            locationComboBox.Location = new Point(458, 636);
+            locationComboBox.Name = "locationComboBox";
+            locationComboBox.Size = new Size(414, 38);
+            locationComboBox.TabIndex = 14;
+            // 
+            // updateButton
+            // 
+            updateButton.BackColor = Color.DarkSeaGreen;
+            updateButton.FlatStyle = FlatStyle.Popup;
+            updateButton.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            updateButton.Location = new Point(458, 740);
+            updateButton.Name = "updateButton";
+            updateButton.Size = new Size(131, 40);
+            updateButton.TabIndex = 15;
+            updateButton.Text = "Update";
+            updateButton.UseVisualStyleBackColor = false;
             // 
             // UserProfile
             // 
@@ -113,17 +178,12 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.MintCream;
             ClientSize = new Size(1622, 1122);
-            Controls.Add(notificationLabel);
-            Controls.Add(locationLabel);
-            Controls.Add(phoneLabel);
-            Controls.Add(emailLabel);
-            Controls.Add(lastNameLabel);
-            Controls.Add(firstNameLabel);
-            Controls.Add(headingLabel);
+            Controls.Add(panel1);
             Name = "UserProfile";
             Text = "User Profile";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -134,6 +194,12 @@
         private Label emailLabel;
         private Label phoneLabel;
         private Label locationLabel;
-        private Label notificationLabel;
+        private Panel panel1;
+        private TextBox lastNameTextBox;
+        private TextBox firstNameTextBox;
+        private TextBox phoneTextBox;
+        private TextBox emailTextBox;
+        private ComboBox locationComboBox;
+        private Button updateButton;
     }
 }
