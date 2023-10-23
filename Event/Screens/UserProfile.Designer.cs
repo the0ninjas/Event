a6cfaf3e53a54a988fb34cd506cf135c0ffbf035
Stanley Tsz Kin Lam <StanleyTszKin.Lam@student.uts.edu.au>
2023-10-23 19:24:12 +1100

    login successful to toppage

--- Event/Screens/UserProfile.Designer.cs
+++ Event/Screens/UserProfile.Designer.cs
@@ -34,23 +34,17 @@
             emailLabel = new Label();
             phoneLabel = new Label();
             locationLabel = new Label();
-            panel1 = new Panel();
-            firstNameTextBox = new TextBox();
-            lastNameTextBox = new TextBox();
-            emailTextBox = new TextBox();
-            phoneTextBox = new TextBox();
-            locationComboBox = new ComboBox();
-            updateButton = new Button();
-            panel1.SuspendLayout();
+            notificationLabel = new Label();
             SuspendLayout();
             // 
             // headingLabel
             // 
             headingLabel.AutoSize = true;
             headingLabel.Font = new Font("Segoe UI Black", 15.000001F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            headingLabel.Location = new Point(154, 75);
+            headingLabel.Location = new Point(103, 97);
+            headingLabel.Margin = new Padding(2, 0, 2, 0);
             headingLabel.Name = "headingLabel";
-            headingLabel.Size = new Size(230, 48);
+            headingLabel.Size = new Size(163, 36);
             headingLabel.TabIndex = 0;
             headingLabel.Text = "User Profile";
             // 
@@ -58,9 +52,10 @@
             // 
             firstNameLabel.AutoSize = true;
             firstNameLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            firstNameLabel.Location = new Point(154, 236);
+            firstNameLabel.Location = new Point(446, 167);
+            firstNameLabel.Margin = new Padding(2, 0, 2, 0);
             firstNameLabel.Name = "firstNameLabel";
-            firstNameLabel.Size = new Size(132, 32);
+            firstNameLabel.Size = new Size(93, 23);
             firstNameLabel.TabIndex = 1;
             firstNameLabel.Text = "First Name";
             // 
@@ -68,9 +63,10 @@
             // 
             lastNameLabel.AutoSize = true;
             lastNameLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            lastNameLabel.Location = new Point(154, 336);
+            lastNameLabel.Location = new Point(446, 233);
+            lastNameLabel.Margin = new Padding(2, 0, 2, 0);
             lastNameLabel.Name = "lastNameLabel";
-            lastNameLabel.Size = new Size(130, 32);
+            lastNameLabel.Size = new Size(91, 23);
             lastNameLabel.TabIndex = 2;
             lastNameLabel.Text = "Last Name";
             // 
@@ -78,9 +74,10 @@
             // 
             emailLabel.AutoSize = true;
             emailLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            emailLabel.Location = new Point(154, 436);
+            emailLabel.Location = new Point(446, 300);
+            emailLabel.Margin = new Padding(2, 0, 2, 0);
             emailLabel.Name = "emailLabel";
-            emailLabel.Size = new Size(165, 32);
+            emailLabel.Size = new Size(116, 23);
             emailLabel.TabIndex = 3;
             emailLabel.Text = "Email Address";
             // 
@@ -88,9 +85,10 @@
             // 
             phoneLabel.AutoSize = true;
             phoneLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            phoneLabel.Location = new Point(154, 536);
+            phoneLabel.Location = new Point(446, 367);
+            phoneLabel.Margin = new Padding(2, 0, 2, 0);
             phoneLabel.Name = "phoneLabel";
-            phoneLabel.Size = new Size(179, 32);
+            phoneLabel.Size = new Size(127, 23);
             phoneLabel.TabIndex = 4;
             phoneLabel.Text = "Phone Number";
             // 
@@ -98,92 +96,42 @@
             // 
             locationLabel.AutoSize = true;
             locationLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
-            locationLabel.Location = new Point(154, 636);
+            locationLabel.Location = new Point(446, 433);
+            locationLabel.Margin = new Padding(2, 0, 2, 0);
             locationLabel.Name = "locationLabel";
-            locationLabel.Size = new Size(107, 32);
+            locationLabel.Size = new Size(75, 23);
             locationLabel.TabIndex = 5;
             locationLabel.Text = "Location";
             // 
-            // panel1
+            // notificationLabel
             // 
-            panel1.Controls.Add(updateButton);
-            panel1.Controls.Add(locationComboBox);
-            panel1.Controls.Add(phoneTextBox);
-            panel1.Controls.Add(emailTextBox);
-            panel1.Controls.Add(lastNameTextBox);
-            panel1.Controls.Add(firstNameTextBox);
-            panel1.Controls.Add(emailLabel);
-            panel1.Controls.Add(headingLabel);
-            panel1.Controls.Add(firstNameLabel);
-            panel1.Controls.Add(locationLabel);
-            panel1.Controls.Add(lastNameLabel);
-            panel1.Controls.Add(phoneLabel);
-            panel1.Location = new Point(304, 45);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(1016, 855);
-            panel1.TabIndex = 7;
-            // 
-            // firstNameTextBox
-            // 
-            firstNameTextBox.Location = new Point(458, 236);
-            firstNameTextBox.Name = "firstNameTextBox";
-            firstNameTextBox.Size = new Size(414, 35);
-            firstNameTextBox.TabIndex = 7;
-            // 
-            // lastNameTextBox
-            // 
-            lastNameTextBox.Location = new Point(458, 336);
-            lastNameTextBox.Name = "lastNameTextBox";
-            lastNameTextBox.Size = new Size(414, 35);
-            lastNameTextBox.TabIndex = 8;
-            // 
-            // emailTextBox
-            // 
-            emailTextBox.Location = new Point(458, 436);
-            emailTextBox.Name = "emailTextBox";
-            emailTextBox.Size = new Size(414, 35);
-            emailTextBox.TabIndex = 9;
-            // 
-            // phoneTextBox
-            // 
-            phoneTextBox.Location = new Point(458, 536);
-            phoneTextBox.Name = "phoneTextBox";
-            phoneTextBox.Size = new Size(414, 35);
-            phoneTextBox.TabIndex = 10;
-            // 
-            // locationComboBox
-            // 
-            locationComboBox.FormattingEnabled = true;
-            locationComboBox.Items.AddRange(new object[] { "Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide", "Canberra", "Hobart", "Darwin" });
-            locationComboBox.Location = new Point(458, 636);
-            locationComboBox.Name = "locationComboBox";
-            locationComboBox.Size = new Size(414, 38);
-            locationComboBox.TabIndex = 14;
-            // 
-            // updateButton
-            // 
-            updateButton.BackColor = Color.DarkSeaGreen;
-            updateButton.FlatStyle = FlatStyle.Popup;
-            updateButton.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            updateButton.Location = new Point(458, 740);
-            updateButton.Name = "updateButton";
-            updateButton.Size = new Size(131, 40);
-            updateButton.TabIndex = 15;
-            updateButton.Text = "Update";
-            updateButton.UseVisualStyleBackColor = false;
+            notificationLabel.AutoSize = true;
+            notificationLabel.Font = new Font("Segoe UI Semibold", 9.857143F, FontStyle.Bold, GraphicsUnit.Point);
+            notificationLabel.Location = new Point(446, 500);
+            notificationLabel.Margin = new Padding(2, 0, 2, 0);
+            notificationLabel.Name = "notificationLabel";
+            notificationLabel.Size = new Size(100, 23);
+            notificationLabel.TabIndex = 6;
+            notificationLabel.Text = "Notification";
             // 
             // UserProfile
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.MintCream;
-            ClientSize = new Size(1622, 1122);
-            Controls.Add(panel1);
+            ClientSize = new Size(1081, 748);
+            Controls.Add(notificationLabel);
+            Controls.Add(locationLabel);
+            Controls.Add(phoneLabel);
+            Controls.Add(emailLabel);
+            Controls.Add(lastNameLabel);
+            Controls.Add(firstNameLabel);
+            Controls.Add(headingLabel);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "UserProfile";
             Text = "User Profile";
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -194,12 +142,6 @@
         private Label emailLabel;
         private Label phoneLabel;
         private Label locationLabel;
-        private Panel panel1;
-        private TextBox lastNameTextBox;
-        private TextBox firstNameTextBox;
-        private TextBox phoneTextBox;
-        private TextBox emailTextBox;
-        private ComboBox locationComboBox;
-        private Button updateButton;
+        private Label notificationLabel;
     }
 }
