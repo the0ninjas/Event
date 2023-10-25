a0d6eecb74ff8d8b96469a196474030d6e98bc35
Stanley Tsz Kin Lam <StanleyTszKin.Lam@student.uts.edu.au>
2023-10-25 18:24:22 +1100

    added user session class and updated the user profile page to fetch user data

--- Event/Screens/UserProfile.cs
+++ Event/Screens/UserProfile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EventManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,11 +16,30 @@ namespace EventManagementSystem.Screens
         public UserProfile()
         {
             InitializeComponent();
+            LoadUserData();
         }
+        private void LoadUserData()
+        {
+            User authenticatedUser = UserSession.AuthenticatedUser;
 
+            if (authenticatedUser != null)
+            {
+                // Populate UI elements with user data
+                firstNameTextBox.Text = authenticatedUser.FirstName;
+                lastNameTextBox.Text = authenticatedUser.LastName;
+                emailTextBox.Text = authenticatedUser.Email;
+                // Add other data as needed
+            }
+        }
         private void firstNameTextBox_TextChanged(object sender, EventArgs e)
         {
+            //User authenticatedUser = UserSession.AuthenticatedUser;
 
+            //if (authenticatedUser != null)
+            //{
+            //    // Populate the TextBox with the user's first name
+            //    firstNameTextBox.Text = authenticatedUser.FirstName;
+            //}
         }
 
         private void lastNameTextBox_TextChanged(object sender, EventArgs e)
@@ -41,5 +61,6 @@ namespace EventManagementSystem.Screens
         {
 
         }
+
     }
 }
