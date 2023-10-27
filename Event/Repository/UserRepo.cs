f840b74edfc04aff5515534351f0bdbaf863b8f5
HPaaron <114461908+HPaaron@users.noreply.github.com>
2023-10-27 15:32:49 +1100

    Created functions for all database related operations in userRepo

--- Event/Repository/UserRepo.cs
+++ Event/Repository/UserRepo.cs
@@ -1,8 +1,10 @@
 ﻿using EventManagementSystem.Models;
 using EventManagementSystem.Utilities;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
@@ -12,44 +14,101 @@ namespace EventManagementSystem.Repository
     public class UserRepo
     {
 
-        private List<User> users = new List<User>();
+        private List<User> Users = new List<User>();
 
-        public static void AddUser(User user)
+
+        public bool createUser(User user)
         {
-            //try
-            //{
+            try
+            {
+                using (var context = new ConnectionFactory())
+                {
+                    User newUser = user;
 
-            //    using (var context = new ConnectionFactory())
-            //    {
-            //        context.Users.Add(user);
-            //        context.SaveChanges();
-            //    }
-            //}
-
-            //catch (Exception ex)
-            //{
-            //    // Display an error message to prompt for correct input
-            //    MessageBox.Show($"Sign-up failed: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //}
+                    context.Users.Add(newUser);
+                    context.SaveChanges();
+                }
+                return true;
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return false;
+            }
         }
 
-        public User GetUserById(int userId)
+        public User getUserByEmail(string email)
         {
-            return null;
+            try
+            {
+                User user = Users.Find(u => u.email == email);
+                return user;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return null;
+            }
         }
 
-        public void UpdateUser(User user)
+        public User updateUser(User updatedUser)
         {
+            try
+            {
+                using (var context = new ConnectionFactory())
+                {
 
+                    User existingUser = Users.Find(u => u.email == updatedUser.email);
+
+                    if (existingUser != null)
+                    {
+                        existingUser.firstName = updatedUser.firstName;
+                        existingUser.lastName = updatedUser.lastName;
+                        existingUser.phoneNumber = updatedUser.phoneNumber;
+                        existingUser.location = updatedUser.location;
+
+                        context.SaveChanges();
+                        return existingUser;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return null; 
+            }
         }
 
-        public void DeleteUser(int userId) 
+        public bool deleteUser(string email) 
         {
-        
+            try
+            {
+                using (var context = new ConnectionFactory())
+                {
+                    User userToDelete = Users.Find(u => u.email == email);
+
+                    if (userToDelete != null)
+                    {
+                        context.Users.Remove(userToDelete);
+                        context.SaveChanges();
+
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return false;
+            }
         }
-         
-        
-        
-        
     }
 }
